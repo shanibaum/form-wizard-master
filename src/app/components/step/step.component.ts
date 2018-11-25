d0ec0f705830e21d0ac383a216c30f6b037e4401
shanibaum <shanibaum2@gmail.com>
2018-11-25 10:20:26 +0200

    skip next back

--- src/app/components/step/step.component.ts
+++ src/app/components/step/step.component.ts
@@ -1,51 +1,60 @@
 import { Component , EventEmitter , Input , OnChanges , OnInit , Output , SimpleChanges , ViewChild } from '@angular/core';
-import {StepDTO} from '../../models/dto/step';
-import {FormBuilderComponent} from '../form-builder/form-builder/form-builder.component';
+import { StepDTO } from '../../models/dto/step';
+import { FormBuilderComponent } from '../form-builder/form-builder/form-builder.component';
 import { FormControl , FormGroup } from '@angular/forms';
 
-@Component({
-  selector: 'app-step',
-  templateUrl: './step.component.html',
-  styleUrls: ['./step.component.scss']
-})
+@Component( {
+  selector : 'app-step' ,
+  templateUrl : './step.component.html' ,
+  styleUrls : [ './step.component.scss' ]
+} )
 export class StepComponent implements OnInit {
   @Input() step: StepDTO;
+  @Input() isFirstStep: boolean;
   @Input() isLastStep: boolean;
   @Input() form: FormGroup;
-  @ViewChild(FormBuilderComponent) formBuilderComponent: FormBuilderComponent;
-  @Output()  IsFormValid: EventEmitter<boolean> = new EventEmitter<boolean>(false);
-  @Output()  wizardSubmitted: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild( FormBuilderComponent ) formBuilderComponent: FormBuilderComponent;
+  @Output() IsFormValid: EventEmitter<boolean> = new EventEmitter<boolean>( false );
+  @Output() wizardSubmitted: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor() {
     this.isLastStep = false;
+    this.isFirstStep = false;
   }
 
   ngOnInit() {
   }
 
   isFormValid() {
-    this.step.stepType.completed  = this.form.valid;
-    this.IsFormValid.emit(this.form.valid);
-    if (this.form.invalid) {
-      this.markAllAsTouched(this.form);
+    if ( this.step.stepType.optional ) {
+      return;
     }
-}
-  submit() {
-    this.wizardSubmitted.emit(true);
+    this.step.stepType.completed = this.form.valid;
+    this.IsFormValid.emit( this.form.valid );
+    if ( this.form.invalid ) {
+      this.markAllAsTouched( this.form );
+    }
   }
 
-  private markAllAsTouched(group: FormGroup ) {
-    Object.keys(group.controls).map((field) => {
-      const control = group.get(field);
-      if (control instanceof FormControl) {
-        if (control.invalid && control.errors['Mandatory']) {
-          control.markAsTouched({ onlySelf: true });
-          control.markAsDirty({ onlySelf: true });
-          control.updateValueAndValidity({ onlySelf: true });
+  submit() {
+    if ( this.form.invalid ) {
+      this.markAllAsTouched( this.form );
+    }
+    this.wizardSubmitted.emit( true );
+  }
+
+  private markAllAsTouched( group: FormGroup ) {
+    Object.keys( group.controls ).map( ( field ) => {
+      const control = group.get( field );
+      if ( control instanceof FormControl ) {
+        if ( control.invalid && control.errors[ 'Mandatory' ] ) {
+          control.markAsTouched( { onlySelf : true } );
+          control.markAsDirty( { onlySelf : true } );
+          control.updateValueAndValidity( { onlySelf : true } );
         }
-      } else if (control instanceof FormGroup) {
-        this.markAllAsTouched(control);
+      } else if ( control instanceof FormGroup ) {
+        this.markAllAsTouched( control );
       }
-    });
+    } );
   }
 }
